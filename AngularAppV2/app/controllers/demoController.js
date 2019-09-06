9914de33cb42987798e215fabb32f2ef21484e7a
unknown <debmalyapaulfbp@gmail.com>
2019-09-06 19:33:50 +0530

    employee search by angularjs added

---
+++ AngularAppV2/app/controllers/demoController.js
@@ -0,0 +1,64 @@
+﻿var app = angular.module('app', ['ngAnimate', 'ngAria', 'ngMessages', 'ngMaterial']);
+app.controller('DemoCtrl', function ($timeout, $q, $log, $scope) {
+
+    
+    $scope.isDisabled = false;
+
+    
+
+
+
+    $scope.employeeArray =
+        [
+            {
+                empId: 10,
+                empName: "Debmalya",
+                gender: "Male",
+                empAddress: "Jadavpur"
+            },
+            {
+                empId: 11,
+                empName: "Chandan",
+                gender: "Male",
+                empAddress: "Garia"
+            },
+            {
+                empId: 12,
+                empName: "Subhendu",
+                gender: "Male",
+                empAddress: "Salt lake"
+            },
+            {
+                empId: 13,
+                empName: "Satyaki",
+                gender: "Male",
+                empAddress: "Hooghly"
+            }
+        ];
+
+
+    
+
+    $scope.searchEmployee = function (text) {
+        $scope.newEmployee = [];
+        for (var i = 0; i < $scope.employeeArray.length; i++) 
+        {
+            if ($scope.employeeArray[i].empName.includes(text)) {
+                $scope.newEmployee.push($scope.employeeArray[i]);
+            }
+        }
+
+        return $scope.newEmployee;
+
+        
+    };
+
+    $scope.selectedEmployeeName = function ($item) {
+        if ($item !== undefined) {
+            $scope.employee = $item;
+        }
+    };
+
+
+    
+});
---
+++ AngularAppV2/app/controllers/studentController.js
@@ -0,0 +1,67 @@
+﻿var app = angular.module('app', ['ngAnimate', 'ngAria', 'ngMessages', 'ngMaterial']);
+app.controller('DemoCtrl1', function ($timeout, $q, $log, $scope) {
+
+
+    $scope.isDisabled = false;
+
+  
+    $scope.studentArray =
+        [
+            {
+                studentId: 104,
+                studentName: "Arup",
+                studentAge: 22,
+                studentDept: "CSE",
+                studentGrade: "O",
+                studentAddress: "USA"
+            },
+            {
+                studentId: 106,
+                studentName: "Kaustuv",
+                studentAge: 24,
+                studentDept: "IT",
+                studentGrade: "A+",
+                studentAddress: "UK"
+            },
+            {
+                studentId: 108,
+                studentName: "Nilanjan",
+                studentAge: 26,
+                studentDept: "ECE",
+                studentGrade: "AA",
+                studentAddress: "India"
+            },
+            {
+                studentId: 110,
+                studentName: "Soumyadeep",
+                studentAge: 23,
+                studentDept: "CSE",
+                studentGrade: "A",
+                studentAddress: "India"
+            }
+        ];
+
+
+  
+    $scope.searchStudents = function (inputText) {
+        $scope.newStudents = [];
+        for (var i = 0; i < $scope.studentArray.length; i++) {
+            if ($scope.studentArray[i].studentName.includes(inputText)) {
+                $scope.newStudents.push($scope.studentArray[i]);
+            }
+        }
+        return $scope.newStudents;
+    };
+
+   
+    $scope.selectedStudentsName = function ($students) {
+        if ($students !== undefined) {
+            $scope.myStudents = $students;
+        }
+    };
+
+
+    
+
+
+});
